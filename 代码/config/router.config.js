5a9a3bc5a8c5671a2fc5c2ee009e3756f12af6fc
xiaosancao <931323908@qq.com>
2019-07-09 22:42:31 +0800

    first

---
+++ 代码/config/router.config.js
@@ -0,0 +1,31 @@
+export default [
+  // user
+  {
+    path: '/user',
+    component: '../layouts/UserLayout',
+    routes: [
+      { path: '/user', redirect: '/user/login' },
+      { path: '/user/login', name: 'login', component: './User/Login' },
+      { path: '/user/register', name: 'register', component: './User/Register' },
+      {
+        path: '/user/register-result',
+        name: 'register.result',
+        component: './User/RegisterResult',
+      },
+      {
+        component: '404',
+      },
+    ],
+  },
+  // app
+  {
+    path: '/',
+    component: '../layouts/BasicLayout',
+    Routes: ['src/pages/Authorized'],
+    routes: [
+      {
+        component: '404',
+      },
+    ],
+  },
+];
